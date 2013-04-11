1678acc7b5cf2b9397b9e6225735aa95994f5088
Woongcheol <woongcheol@gmail.com>
2013-04-10 20:06:14 -0400

    Implement Logic.huffman

--- logic.rb
+++ logic.rb
@@ -14,6 +14,43 @@ module Logic
     end
   end
 
+  # Examples
+  #
+  #   huffman_build_tree({ a: 45, b: 13, c: 12, d: 16, e: 9, f: 5 })
+  #   # => [:a, [[:c, :b], [[:f, :e], :d]]]
+  def self.huffman_build_tree(fs)
+    tree = fs.to_a.map { |f| f.reverse }.sort
+    until tree.size == 1
+      m = tree.shift
+      n = tree.shift
+      tree.unshift [m.first + n.first, [m.last, n.last]]
+      tree.sort!
+    end
+    tree[0][1]
+  end
+
+  # Examples
+  #
+  #   huffman_assign_code([:a, [[:c, :b], [[:f, :e], :d]]])
+  #   # => [:a, "0", :b, "101", :c, "001", :d, "111", :e, "1011", :f, "0011"]
+  def self.huffman_assign_code(node, prefix = "")
+    if node.respond_to?(:first)
+      [
+        huffman_assign_code(node.first, prefix + "0"),
+        huffman_assign_code(node.last, prefix + "1")
+      ].sort.flatten
+    else
+      [node, prefix]
+    end
+  end
+
+  # Examples
+  #
+  #   huffman({ a: 45, b: 13, c: 12, d: 16, e: 9, f: 5 })
+  #   # => {:a=>"0", :b=>"101", :c=>"100", :d=>"111", :e=>"1101", :f=>"1100"}
   def self.huffman(fs)
+    tree = huffman_build_tree(fs)
+    codes = huffman_assign_code(tree)
+    Hash[*codes]
   end
 end
