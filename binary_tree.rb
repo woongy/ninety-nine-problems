ef1c15bfc0ac23f6bc816349a0dd3c810a92fee4
Woongcheol <woongcheol@gmail.com>
2013-04-12 22:27:57 -0400

    Implement some BinaryTree functions and specs

--- binary_tree.rb
+++ binary_tree.rb
@@ -11,4 +11,165 @@ module BinaryTree
       tree?(left) && tree?(right)
     end
   end
+
+  def self.cbal_trees(n, sym = :x)
+  end
+
+  def self.mirror?(t, u)
+    root_t, left_t, right_t = t
+    root_u, left_u, right_u = u
+
+    if root_t == :nil && root_u == :nil
+      true
+    elsif [left_t, right_t, left_u, right_u].any?(&:nil?)
+      false
+    else
+      mirror?(left_t, right_u) && mirror?(right_t, left_u)
+    end
+  end
+
+  def self.symmetric?(t)
+    root, left, right = t
+
+    if root == :nil
+      true
+    else
+      mirror?(left, right)
+    end
+  end
+
+  def self.add(t, value)
+    root, left, right = t
+
+    if root == :nil
+      [value, :nil, :nil]
+    else
+      if value < root
+        [root, add(left, value), right]
+      elsif root < value
+        [root, left, add(right, value)]
+      end
+    end
+  end
+
+  def self.construct(values)
+    values.inject(:nil) { |t, value| add(t, value) }
+  end
+
+  def self.sym_cbal_trees(n, sym = :x)
+  end
+
+  def self.hbal_trees(h, sym = :x)
+  end
+
+  def self.hbal_min_nodes(h)
+  end
+
+  def self.hbal_max_height(n)
+  end
+
+  def self.hbal_tree_nodes(n, sym = :x)
+  end
+
+  def self.count_leaves(t)
+    leaves(t).size
+  end
+
+  def self.leaves(t)
+    root, left, right = t
+
+    if root == :nil
+      []
+    elsif left == :nil && right == :nil
+      [root]
+    else
+      leaves(left) + leaves(right)
+    end
+  end
+
+  def self.internals(t)
+    t.flatten.reject { |node| node == :nil } - leaves(t)
+  end
+
+  def self.at_level(t, n)
+    root, left, right = t
+
+    if n == 1
+      root == :nil ? [] : [root]
+    else
+      at_level(left, n - 1) + at_level(right, n - 1)
+    end
+  end
+
+  def self.complete_binary_tree(n)
+  end
+
+  def self.layout_binary_tree(t, method)
+  end
+
+  def self.tree_to_string(t)
+    root, left, right = t
+
+    if root == :nil
+      ""
+    elsif left == :nil && right == :nil
+      "#{root}"
+    else
+      "#{root}(#{tree_to_string(left)},#{tree_to_string(right)})"
+    end
+  end
+
+  def self.string_to_tree(s)
+  end
+
+  def self.tree_string(t_or_s)
+    case t_or_s
+    when Array
+      tree_to_string t_or_s
+    when String
+      string_to_tree t_or_s
+    end
+  end
+
+  def self.preorder(t)
+    root, left, right = t
+
+    if root == :nil
+      ""
+    elsif left == :nil && right == :nil
+      "#{root}"
+    else
+      "#{root}#{preorder(left)}#{preorder(right)}"
+    end
+  end
+
+  def self.inorder(t)
+  end
+
+  def self.pre_in_tree(p, i)
+  end
+
+  def self.tree_to_dotstring(t)
+    root, left, right = t
+
+    if root == :nil
+      "."
+    elsif left == :nil && right == :nil
+      "#{root}.."
+    else
+      "#{root}#{tree_to_dotstring(left)}#{tree_to_dotstring(right)}"
+    end
+  end
+
+  def self.dotstring_to_tree(s)
+  end
+
+  def self.tree_dotstring(t_or_s)
+    case t_or_s
+    when Array
+      tree_to_dotstring t_or_s
+    when String
+      dotstring_to_tree t_or_s
+    end
+  end
 end
--- spec/binary_tree_spec.rb
+++ spec/binary_tree_spec.rb
@@ -28,4 +28,218 @@ describe BinaryTree do
       end
     end
   end
+
+  describe ".cbal_trees" do
+
+    let(:expected) do
+      [
+        [:x, [:x, :nil, :nil], [:x, :nil, [:x, :nil, :nil]]],
+        [:x, [:x, :nil, :nil], [:x, [:x, :nil, :nil], :nil]],
+        [:x, [:x, :nil, [:x, :nil, :nil]], [:x, :nil, :nil]],
+        [:x, [:x, [:x, :nil, :nil], :nil], [:x, :nil, :nil]]
+      ]
+    end
+
+    it "generates a list of completely balanced binary trees for a given number of nodes" do
+      expect(BinaryTree.cbal_trees(4)).to eq expected
+    end
+  end
+
+  describe ".symmetric?" do
+
+    context "when given a symmetric tree" do
+
+      let(:t) { [:a, [:b, [:c, :nil, :nil], :nil], [:d, :nil, [:e, :nil, :nil]]] }
+
+      it "returns true" do
+        expect(BinaryTree.symmetric?(t)).to be_true
+      end
+    end
+
+    context "when given an asymmetric tree" do
+
+      let(:t) { [:a, [:b, [:c, :nil, :nil], :nil], [:d, [:e, :nil, :nil], :nil]] }
+
+      it "returns false" do
+        expect(BinaryTree.symmetric?(t)).to be_false
+      end
+    end
+  end
+
+  describe ".construct" do
+
+    let(:input) { [3, 2, 5, 7, 1] }
+    let(:expected) { [3, [2, [1, :nil, :nil], :nil], [5, :nil, [7, :nil, :nil]]] }
+
+    it "constructs a binary search from a list of integer numbers" do
+      expect(BinaryTree.construct(input)).to eq expected
+    end
+  end
+
+  describe ".sym_cbal_trees" do
+
+    it "generates a list of symmetric and completely balanced binary trees for a given number of nodes"
+  end
+
+  describe ".hbal_trees" do
+
+    it "generates a list of height-balanced binary trees for a given height"
+  end
+
+  describe ".hbal_min_nodes" do
+
+    it "finds the minimum number of nodes in a height-balanced binary tree of a given height"
+  end
+
+  describe ".hbal_max_height" do
+
+    it "finds the maximum height of a height-balanced binary tree with a given number of nodes"
+  end
+
+  describe ".hbal_tree_nodes" do
+
+    it "generates a list of height-balanced binary trees with a given number of nodes"
+  end
+
+  describe ".count_leaves" do
+
+    let(:t) do
+      [:a,
+        [:b, [:d, :nil, :nil], [:e, :nil, :nil]],
+        [:c, :nil, [:f, [:g, :nil, :nil], :nil]]]
+    end
+
+    it "counts the leaves of a binary tree" do
+      expect(BinaryTree.count_leaves(t)).to eq 3
+    end
+  end
+
+  describe ".leaves" do
+
+    let(:t) do
+      [:a,
+        [:b, [:d, :nil, :nil], [:e, :nil, :nil]],
+        [:c, :nil, [:f, [:g, :nil, :nil], :nil]]]
+    end
+
+    it "collects the leaves of a binary tree in a list" do
+      expect(BinaryTree.leaves(t)).to eq %i(d e g)
+    end
+  end
+
+  describe ".internals" do
+
+    let(:t) do
+      [:a,
+        [:b, [:d, :nil, :nil], [:e, :nil, :nil]],
+        [:c, :nil, [:f, [:g, :nil, :nil], :nil]]]
+    end
+
+    it "collects the inernal nodes of a binary tree in a list" do
+      expect(BinaryTree.internals(t)).to eq %i(a b c f)
+    end
+  end
+
+  describe ".at_level" do
+
+    let(:t) do
+      [:a,
+        [:b, [:d, :nil, :nil], [:e, :nil, :nil]],
+        [:c, :nil, [:f, [:g, :nil, :nil], :nil]]]
+    end
+
+    it "collects the nodes at a given level in a list" do
+      expect(BinaryTree.at_level(t, 3)).to eq %i(d e f)
+    end
+  end
+
+  describe ".complete_binary_tree" do
+
+    it "constructs a complete binary tree with a given number of nodes"
+  end
+
+  describe ".layout_binary_tree" do
+
+  end
+
+  describe ".tree_string" do
+
+    context "when given a tree" do
+
+      let(:t) do
+        [:a,
+          [:b, [:d, :nil, :nil], [:e, :nil, :nil]],
+          [:c, :nil, [:f, [:g, :nil, :nil], :nil]]]
+      end
+
+      it "returns its string representation" do
+        expect(BinaryTree.tree_string(t)).to eq "a(b(d,e),c(,f(g,)))"
+      end
+    end
+
+    context "when given a string" do
+
+      let(:expected) do
+        ["a",
+          ["b", ["d", :nil, :nil], ["e", :nil, :nil]],
+          ["c", :nil, ["f", ["g", :nil, :nil], :nil]]]
+      end
+
+      it "constructs the tree represented by it" do
+        expect(BinaryTree.tree_string("a(b(d,e),c(,f(g,)))")).to eq expected
+      end
+    end
+  end
+
+  describe ".preorder" do
+
+    let(:t) do
+      ["a",
+        ["b", ["d", :nil, :nil], ["e", :nil, :nil]],
+        ["c", :nil, ["f", ["g", :nil, :nil], :nil]]]
+    end
+
+    it "constructs the preorder sequence of a given binary tree" do
+      expect(BinaryTree.preorder(t)).to eq "abdecfg"
+    end
+  end
+
+  describe ".inorder" do
+
+    it "constructs the inorder sequence of a given binary tree"
+  end
+
+  describe ".pre_in_tree" do
+
+    it "constructs the binary tree that has a given preorder sequence and a inorder sequence"
+  end
+
+  describe ".tree_dotstring" do
+
+    context "when given a tree" do
+
+      let(:t) do
+        [:a,
+          [:b, [:d, :nil, :nil], [:e, :nil, :nil]],
+          [:c, :nil, [:f, [:g, :nil, :nil], :nil]]]
+      end
+
+      it "returns its dotstring representation" do
+        expect(BinaryTree.tree_dotstring(t)).to eq "abd..e..c.fg..."
+      end
+    end
+
+    context "when given a dotstring" do
+
+      let(:expected) do
+        ["a",
+          ["b", ["d", :nil, :nil], ["e", :nil, :nil]],
+          ["c", :nil, ["f", ["g", :nil, :nil], :nil]]]
+      end
+
+      it "constructs the tree represented by it" do
+        expect(BinaryTree.tree_dotstring("abd..e..c.fg...")).to eq expected
+      end
+    end
+  end
 end
